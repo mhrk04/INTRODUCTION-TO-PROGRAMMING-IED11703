f456a2ba82ba05596fe1410d0d5901c4278a4506
mhrk04 <haziqrohaizan@gmail.com>
2022-08-11 13:08:27 +0800

    add exercise 5; Complete

--- 20220811/Algorithms5.cpp
+++ 20220811/Algorithms5.cpp
@@ -22,7 +22,28 @@ cout<< R"(
 
 )"<<"\n";
 
-// Average speed =
+// Average speed 
 
+// set variable
+double DistanceTraveled;
+double TimeTaken;
+double AvgSpd;
+
+cout<<"Average speed Calculator (KM/H)"<<endl;
+// get distance traveled
+cout<<"Please Insert Distance Traveled in (KM) "<<endl;
+cin>>DistanceTraveled;
+// get time
+cout<<"Please Insert Time Taken in HOUR "<<endl;
+cin>>TimeTaken;
+
+// calculate
+AvgSpd=DistanceTraveled/TimeTaken;
+// show all value that user input
+cout<<"The Value that you keyin :"<<endl;
+cout<<"Distance Traveled : "+to_string(DistanceTraveled)+" KM"<<endl;
+cout<<"Time Taken : "+to_string(TimeTaken)+" Hour"<<endl;
+// Print result
+cout<<"Average speed = "+to_string(AvgSpd)+" KM/H"<<endl;
   return 0;
 }
