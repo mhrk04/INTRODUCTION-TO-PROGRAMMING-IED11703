81fbd5cb02f2d01063becf5a8e9922a9b2b51b81
mhrk04 <haziqrohaizan@gmail.com>
2022-08-18 09:52:25 +0800

    add task 1

---
+++ 20220818/README.md
@@ -0,0 +1,7 @@
+# Lab L01B02
+
+> 18 August 2022
+
+## Task
+
+1. Input char to show day
---
+++ 20220818/Task1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  char Day;
+  cout<< R"(
+    U = Sunday
+    M = Monday
+    T = Tuesday
+    W = Wednesday
+    R = Thursday
+    F = Friday
+    S = Saturday
+
+    Please enter the day according character above:
+  )"<<endl;
+
+  cin>>Day;
+  if (Day == 'U')
+  {
+    cout<<"It is Sunday today \n";
+  }else if (Day == 'M')
+  {
+  cout<<"It is Monday today \n";
+  }else if (Day == 'T')
+  {
+  cout<<"It is Tuesday today \n";
+  }else if (Day == 'W')
+  {
+  cout<<"It is Wednesday today \n";
+  }else if (Day == 'R')
+  {
+  cout<<"It is Thursday today \n";
+  }else if (Day == 'F')
+  {
+  cout<<"It is Friday today \n";
+  }else if (Day == 'S')
+  {
+  cout<<"It is Saturday today \n";
+  }else
+  {
+    cout<< "Character not valid \n";
+  }
+  
+  
+  
+
+  return 0;
+}
