4d0549c67dc51ceebb1a29b92bafcb1acc3e7340
mhrk04 <haziqrohaizan@gmail.com>
2022-08-25 10:30:20 +0800

    Guessing game

---
+++ 20220825/task2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  double userIN,Ans = 19;
+
+  cout<<"Number guessing Game"<<endl;
+  cout<<"User Only have 10 try"<<endl;
+int i = 1;
+  while ( i <=10 )
+  {
+    cout<<"Please Guess the Number"<<endl;
+    cin>>userIN;
+    if (userIN == Ans)
+    {
+      cout<<"Great. That's true. It's "+to_string(Ans)<<endl;
+      break;
+    }else if (i == 10)
+    {
+      cout<<"Game Over"<<endl;
+    }
+    else
+    {
+      cout<<"Wrong!! Try again!!"<<endl;
+    }
+    i++;
+  }
+  
+  cout<<"Game End"<<endl;
+}
