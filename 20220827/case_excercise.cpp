f322bded2853bda338e01db4cffe6c589ade2b5e
mhrk04 <haziqrohaizan@gmail.com>
2022-08-28 21:58:07 +0800

    add grade point

--- 20220827/case_excercise.cpp
+++ 20220827/case_excercise.cpp
@@ -11,34 +11,34 @@ int main()
   switch (UserIN)
   {
   case 'A':
-    cout << "4.0" << endl;
+    cout << "Your grade point is 4.0" << endl;
     break;
   case 'a':
-    cout << "4.0" << endl;
+    cout << "Your grade point is 4.0" << endl;
     break;
   case 'B':
-    cout << "2.7" << endl;
+    cout << "Your grade point is 2.7" << endl;
     break;
   case 'b':
-    cout << "2.7" << endl;
+    cout << "Your grade point is 2.7" << endl;
     break;
   case 'C':
-    cout << "2.0" << endl;
+    cout << "Your grade point is 2.0" << endl;
     break;
   case 'c':
-    cout << "2.0" << endl;
+    cout << "Your grade point is 2.0" << endl;
     break;
   case 'D':
-    cout << "1.0" << endl;
+    cout << "Your grade point is 1.0" << endl;
     break;
   case 'd':
-    cout << "1.0" << endl;
+    cout << "Your grade point is 1.0" << endl;
     break;
   case 'E':
-    cout << "0.0" << endl;
+    cout << "Your grade point is 0.0" << endl;
     break;
   case 'e':
-    cout << "0.0" << endl;
+    cout << "Your grade point is 0.0" << endl;
     break;
   default:
     cout << "Please insert the right grade" << endl;
