7aac9bb9ab085fa2970202d8e32d39533b698f4a
mhrk04 <haziqrohaizan@gmail.com>
2022-08-28 18:51:03 +0800

    modify something

--- 20220827/loop_excercise.cpp
+++ 20220827/loop_excercise.cpp
@@ -3,14 +3,23 @@ using namespace std;
 int main()
 {
   // set var
+  cout << R"(
+     Every hour the cashier is required to update the total sales of the hour 
+     to keep track of the cafe's performance.
+     (10 am to 10 pm)
+    )"
+       << "\n";
   double UserIN, sum;
-
-  while (true)
+  // 10 am to 10 pm is 12 hour , so user need to input 12 times
+  int i = 1;
+  while (i <= 12)
   {
-    cout << "Please insert the total sales from last hour : RM ";
+    cout << "Please insert the total sales from the last hour : RM ";
     cin >> UserIN;
+    sum += UserIN;
     cout << "Total sales : RM " + to_string(sum) << endl;
+    i++;
   }
-
+  cout << "Total sales for today : RM " + to_string(sum);
   return 0;
 }
