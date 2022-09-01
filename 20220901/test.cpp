5d57178d855957278bb9aaee5858375f7e84a991
mhrk04 <haziqrohaizan@gmail.com>
2022-09-01 11:08:38 +0800

    if..else test

--- 20220901/test.cpp
+++ 20220901/test.cpp
@@ -26,5 +26,35 @@ int main()
   cout << "==================================================" << endl;
   cout << "Hi " + Name + "," << endl;
   cout << "Your age currently is " + to_string(age) + " years old." << endl;
+
+  // ready for next question
+  // question 2
+  cout << "-----------------------------" << endl;
+
+  if (Yearofbirth < 1946)
+  {
+    cout << "Silent Generation" << endl;
+  }
+  else if (Yearofbirth > 1946 && Yearofbirth < 1964)
+  {
+    cout << "Baby Boomers" << endl;
+  }
+  else if (Yearofbirth > 1965 && Yearofbirth < 1980)
+  {
+    cout << "Generation X" << endl;
+  }
+  else if (Yearofbirth > 1981 && Yearofbirth < 1995)
+  {
+    cout << "Millenials" << endl;
+  }
+  else if (Yearofbirth > 1996 && Yearofbirth < 2010)
+  {
+    cout << "Generation Z" << endl;
+  }
+  else if (Yearofbirth > 2011 && Yearofbirth < 2025)
+  {
+    cout << "Generation alpha" << endl;
+  }
+
   return 0;
 }
